e3aebcc11501986c805d8a96d6c0892c49436d38
Guilherme Ganzarolli <guilhermegalmeida@hotmail.com>
2024-12-30 22:26:54 -0300

    Conseguimos já cadastrar usuários no banco de dados Mongodb

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,29 @@ const dotenv = require('dotenv')
 dotenv.config()
 //Mongoose
 const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGOOSE_LINK)
+
+const User = mongoose.model('User',{
+    name: String,
+    email: String,
+    password: String
+})
 
 const port = process.env.PORT
 
 
 app.get('/', (req, res)=>{
     res.send('Hello world')
+})
+
+app.post("/", async (req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    await user.save()
+    res.send(user)
 })
 
 app.listen(port,()=>{
